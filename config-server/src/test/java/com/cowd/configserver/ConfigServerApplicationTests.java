53558d837000ef8dfe44ad47fc8ad750947eb8b8
cowd90 <tienn.dungg.980@gmail.com>
2024-05-29 11:46:10 +0700

    impl customer service

---
+++ config-server/src/test/java/com/cowd/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cowd.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ customer/src/main/java/com/cowd/ecommerce/customer/CustomerService.java
@@ -0,0 +1,73 @@
+package com.cowd.ecommerce.customer;
+
+import com.cowd.ecommerce.exception.CustomerNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.String.format;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    public String createCustomer(CustomerRequest request) {
+        var customer = customerRepository.save(customerMapper.toCustomer(request));
+        return customer.getId();
+    }
+
+    public void updateCustomer(CustomerRequest request) {
+        var customer = customerRepository.findById(request.id())
+                .orElseThrow(() -> new CustomerNotFoundException(
+                        format("Cannot update customer:: No customer found with the provided ID:: %s", request.id())
+                ));
+
+        mergerCustomer(customer, request);
+        customerRepository.save(customer);
+    }
+
+    private void mergerCustomer(Customer customer, CustomerRequest request) {
+        if (StringUtils.isBlank(request.firstname())) {
+            customer.setFirstname(request.firstname());
+        }
+        if (StringUtils.isBlank(request.lastname())) {
+            customer.setLastname(request.lastname());
+        }
+        if (StringUtils.isBlank(request.email())) {
+            customer.setEmail(request.email());
+        }
+        if (request.email() != null) {
+            customer.setAddress(request.address());
+        }
+    }
+
+    public List<CustomerResponse> findAllCustomers() {
+        return customerRepository.findAll()
+                .stream()
+                .map(customerMapper::fromCustomer)
+                .collect(Collectors.toList());
+    }
+
+    public Boolean existsById(String customerId) {
+        return customerRepository.findById(customerId)
+                .isPresent();
+    }
+
+    public CustomerResponse findById(String customerId) {
+        return customerRepository.findById(customerId)
+                .map(customerMapper::fromCustomer)
+                .orElseThrow(() -> new CustomerNotFoundException(
+                        format("No customer found with the provided ID:: %s", customerId)
+                ));
+    }
+
+    public void deleteCustomer(String customerId) {
+        customerRepository.deleteById(customerId);
+    }
+}
